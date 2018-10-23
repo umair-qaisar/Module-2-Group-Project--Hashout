ebfe34f45766614515411e5a3c48593dc44f3dcf
Patrick <ctrlaltpat@gmail.com>
2018-10-23 17:24:54 +0100

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    
+  end
+
+  def new
+    # register
+  end
+
+
+  private
+
+  def user_params
+    # params.require(:user).permit()
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
+
+  root to: 'static#home'
+
+  get '/about', to: 'static#about'
+
   resources :functions
   resources :locations
   resources :languages
-  resources :users
+  resources :users, only: [:index, :show, :create, :edit, :update, :delete]
+
+  # users
+  get '/register', to: 'users#new', as: 'register'
+
+
+  # session functionality
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create', as: 'sessions'
+  post '/logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
