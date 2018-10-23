d4b448b6c004d5b44a93354ba1b06b40e69aabf2
github username <github email address>
2018-10-23 15:50:37 +0100

    added index/show page for location/languages, added basic attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Language.create(name: "Ruby", description: "easy", creator: "Mr X" )
+Language.create(name: "JavaScript", description: "making some games", creator: "Mr Y" )
+Language.create(name: "Python", description: "user for numbers", creator: "Mr Z" )
+Language.create(name: "SQL", description: "data based", creator: "Mr A" )
