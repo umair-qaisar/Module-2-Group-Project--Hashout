af28885c72f0df74752eafa47f9e7b6e2dbef763
github username <github email address>
2018-10-25 14:43:34 +0100

    umair

--- app/models/language.rb
+++ app/models/language.rb
@@ -6,4 +6,20 @@ class Language < ApplicationRecord
     self.users.count
   end
 
+  def number_of_locations #fun fact about the number of locations it has been studied in.
+    total = 0
+    self.users.each do |user|
+      user.locations.each do |locations|
+      total += 1
+      end
+    end
+    total
+  end
+
+  def names_of_locations #fun fact about the number of locations it has been studied in
+      locations.each do |location|
+      location.name
+      end
+    end
+
 end
